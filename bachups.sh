c36143e304d67e7ee5c06d71935c6bb5c71e9fe1
yfma2010 <yfma2010@gmail.com>
2015-11-06 20:52:08 +0800

    增加prefix

--- bachups.sh
+++ bachups.sh
@@ -1,6 +1,6 @@
-mkdir /backups
+mkdir -p /backups
 date_str=`date +"%Y-%m-%d"`
-tar -zcpf /backups/tar_backup_${date_str}_img.tar.gz \
+tar -zcpf /backups/$1_backup_${date_str}_img.tar.gz \
  --directory=/ --exclude=proc --exclude=sys\
  --exclude=dev/pts --exclude=/lost+found\
- --exclude=backups .  2>>/backups/tar_backup_${date_str}_img.tar.gz.log
+ --exclude=backups .  2>>/backups/$1_backup_${date_str}_img.tar.gz.log
