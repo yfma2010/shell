7dcc4dfebd79159e7b28c29c7ef185dbf4fba5f6
yfma2010 <yfma2010@gmail.com>
2015-11-08 11:50:02 +0800

    installos.sh 更新

--- installos.sh
+++ installos.sh
@@ -2,7 +2,7 @@
 ############### seting  ###################
 disk="/dev/sdb"
 os_file_dir=`pwd`
-tar_file_name="debian_min_restore_backup_2015-11-06_img.tar.gz"
+tar_file_name="xorg_gtk_dev_backup_2015-11-08_img.tar.gz"
 grub_name="grub"
 ###########################################
 tar_file="${os_file_dir}/${tar_file_name}"
@@ -38,10 +38,13 @@ tar   -xpf  ${tar_file}  &&
 echo "end tar " ${tar_file}  
 mkdir -p /mnt/proc 
 mkdir -p /mnt/dev/pts
+mknod -m 600 /mnt/dev/console c 5 1
+mknod -m 666 /mnt/dev/null c 1 3
 mkdir -p /mnt/sys
 grub-install $disk --root-directory=/mnt
 sync  
-
+cd /
+umount /mnt
 }
 
 
