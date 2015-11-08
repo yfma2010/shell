536ca69e396dcbc77247132c2545e647361cb813
yfma2010 <yfma2010@gmail.com>
2015-11-08 11:37:26 +0800

    tar exclude bug

--- bachups.sh
+++ bachups.sh
@@ -1,6 +1,7 @@
 mkdir -p /backups
 date_str=`date +"%Y-%m-%d"`
-tar -zcpf /backups/$1_backup_${date_str}_img.tar.gz \
- --directory=/ --exclude=proc --exclude=sys\
- --exclude=dev/pts --exclude=/lost+found\
- --exclude=backups .  2>>/backups/$1_backup_${date_str}_img.tar.gz.log
+cd /
+tar -zcpf ./backups/$1_backup_${date_str}_img.tar.gz \
+ ./ --exclude=./proc --exclude=./sys\
+ --exclude=./dev --exclude=./lost+found\
+ --exclude=./backups   2>/backups/$1_backup_${date_str}_img.tar.gz.log
